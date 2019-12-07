6eb410b04be5df1f628fdfb8e2a294723bd19d85
Lina Uribe <lina8452@gmail.com>
2019-12-07 05:32:03 -0500

    fixed

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title> Prueba de PHP HEROKU </title>
  </head>
  <body>
- <?php echo '<p> Hola Lina </p>'; ?>
+ <?php
+echo "Variable $saludo: $HTTP_GET_VARS["saludo"] <br>n";
+echo "Variable $saludo: $saludo <br>n";
+echo '<p> Hola Lina </p>'; ?>
  </body>
 </html>
